2f3939ba46237cf079c8bef8aaedbb39b8f5adc1
OAGB Dev <dev@oagb.gw>
2026-03-22 19:50:57 +0000

    style(agenda, banner): fix negative margins destroying mobile agenda and completely redesign registration banner into a modern black glassmorphism layout

--- includes/banner-inscricao.php
+++ includes/banner-inscricao.php
@@ -1,19 +1,23 @@
-<!-- Seção de Inscrição na Ordem -->
 <section class="banner-inscricao mt-5">
-    <div class="banner-inscricao-container container">
-        <div class="banner-left animate__animated animate__fadeInLeft">
-            <p class="texto-conteudo mb-3 banner-p-text">Consulte os documentos necessários para realizar a sua inscrição na Ordem dos Advogados da Guiné-Bissau</p>
-            <a href="inscricao-ordem.php" class="banner-btn-link">
-                <div class="banner-btn-wrapper">
-                    <span class="btn-text">SAIBA MAIS</span>
-                    <div class="btn-arrow-only">
-                        <i class="bi bi-arrow-right"></i>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="banner-right animate__animated animate__fadeInRight">
-            <h2 class="titulo-inscricao">Inscrição na <br> Ordem</h2>
+    <!-- Círculos decorativos à esquerda -->
+    <div class="banner-circle circle-1"></div>
+    <div class="banner-circle circle-2"></div>
+    <div class="banner-circle circle-3"></div>
+    <div class="banner-circle circle-4"></div>
+
+    <div class="container container-content h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-lg-5 col-md-1 d-none d-md-block">
+                <!-- Espaço vazio à esquerda sobre os círculos -->
+            </div>
+            <div class="col-lg-7 col-md-11 text-section z-index-2 animate__animated animate__fadeInRight">
+                <h2 class="titulo-inscricao">Inscrição na<br>Ordem</h2>
+                <p class="texto-conteudo mb-4 mt-3">Consulte os documentos necessários para realizar a sua inscrição na Ordem dos Advogados da Guiné-Bissau.</p>
+                <a href="inscricao-ordem.php" class="banner-pill-btn">
+                    <span class="btn-text">Saiba Mais</span>
+                    <div class="btn-orb"></div>
+                </a>
+            </div>
         </div>
     </div>
 </section>
